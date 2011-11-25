d6e70933b4f38d7d3cc341bc922369461964bdd8
Karl J. Ots <kjots@kjots.org>
2011-11-25 16:30:10 +1100

    [gin-support] Added deferred entry point and entry gin module.

---
+++ gin-support/src/main/java/org/kjots/lib/gwt/gin/client/entry/DeferredEntryGinModule.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright © 2011 Karl J. Ots <kjots@kjots.org>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kjots.lib.gwt.gin.client.entry;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.inject.Provides;
+
+/**
+ * Deferred Entry Gin Module.
+ * <p>
+ * Created: 25th November 2011.
+ *
+ * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
+ * @since 1.0
+ */
+public abstract class DeferredEntryGinModule extends EntryGinModule {
+  /**
+   * Provide the entry point.
+   *
+   * @param deferredEntryPoint The deferred entry point.
+   * @return The entry point.
+   */
+  @Provides
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  protected final EntryPoint provideEntryPoint(final DeferredEntryPoint deferredEntryPoint) {
+    return new EntryPoint() {
+      @Override
+      public void onModuleLoad(final EntryGinjector ginjector) {
+        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+          @Override
+          public void execute() {
+            deferredEntryPoint.onModuleLoad(ginjector);
+          }
+        });
+      }
+    };
+  }
+}
