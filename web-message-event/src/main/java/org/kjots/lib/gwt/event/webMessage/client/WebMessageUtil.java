3c43cee2760d1406b18f530f84054265e441e84e
Karl J. Ots <kjots@kjots.org>
2012-08-05 17:37:04 +1000

    [web-message-event] Updated to log posting of messages.

--- web-message-event/src/main/java/org/kjots/lib/gwt/event/webMessage/client/WebMessageUtil.java
+++ web-message-event/src/main/java/org/kjots/lib/gwt/event/webMessage/client/WebMessageUtil.java
@@ -55,6 +55,8 @@ public class WebMessageUtil {
    * @param transfer The transfer.
    */
   public static native void postMessage(JavaScriptObject window, JsAny<?> message, String targetOrigin, JsArray<JavaScriptObject> transfer) /*-{
+    @org.kjots.lib.gwt.event.webMessage.client.WebMessageUtil::logPostMessage(Lcom/google/gwt/core/client/JavaScriptObject;Lorg/kjots/lib/gwt/js/util/client/JsAny;Ljava/lang/String;Lcom/google/gwt/core/client/JsArray;)(window, message, targetOrigin, transfer);
+    
     window.postMessage(message.value, targetOrigin, transfer);
   }-*/;
 
@@ -115,6 +117,25 @@ public class WebMessageUtil {
         };
       }-*/;
     });
+  }
+  
+  /**
+   * Log the posting of the given message.
+   * 
+   * @param window The window.
+   * @param message The message.
+   * @param targetOrigin The target origin.
+   * @param transfer The transfer.
+   */
+  static void logPostMessage(JavaScriptObject window, JsAny<?> message, String targetOrigin, JsArray<JavaScriptObject> transfer) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("Posting message: window={}, message={}, targetOrigin={}, transfer={}", new Object[] {
+        getWindowUrl(window),
+        JSON.stringify(message),
+        targetOrigin,
+        transfer
+      });
+    }
   }
   
   /**
